d5060cc30b0ce2fcfbf12270dcd0f1d5d7bf5679
littleee <liubochao_job@126.com>
2020-05-17 17:10:38 +0800

    home page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import './App.css';
 import { Home } from '../Home';
+import logo from './logo.png';
+import { Layout } from 'antd';
 
+const { Header } = Layout;
 function App() {
   return (
-    <div>
+    <div className="app">
+    <Header className="header">
+      <img src={logo} alt="logo" className="logo"/>
+    </Header>
       <Home />
     </div>
   );
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,80 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
-import logo from './logo.png';
+import footerIcon from './footer.png';
 import { Layout, Row, Divider, Card, Typography, Col } from 'antd';
 import { BannerAnimation } from '../../components/BannerAnimation';
 import { LineChart } from '../../components/LineChart';
+import axios from 'axios';
+import echarts from 'echarts'
 
 const { Title, Text } = Typography;
-const { Header, Content, Footer } = Layout;
-const option = {
-    title: {
-        text: '收益曲线'
-    },
-    tooltip: {
-        trigger: 'axis'
-    },
-    legend: {
-        data: ['本产品', '持有BTC']
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-    },
-    toolbox: {
-        feature: {
-            saveAsImage: {}
-        }
-    },
-    xAxis: {
-        type: 'time',
-    },
-    yAxis: {
-        type: 'value'
-    },
-    series: [
-        {
-            name: '邮件营销',
-            type: 'line',
-            stack: '总量',
-            data: [120, 132, 101, 134, 90, 230, 210]
-        },
-        {
-            name: '联盟广告',
-            type: 'line',
-            stack: '总量',
-            data: [220, 182, 191, 234, 290, 330, 310]
-        },
-        {
-            name: '视频广告',
-            type: 'line',
-            stack: '总量',
-            data: [150, 232, 201, 154, 190, 330, 410]
-        },
-        {
-            name: '直接访问',
-            type: 'line',
-            stack: '总量',
-            data: [320, 332, 301, 334, 390, 330, 320]
-        },
-        {
-            name: '搜索引擎',
-            type: 'line',
-            stack: '总量',
-            data: [820, 932, 901, 934, 1290, 1330, 1320]
-        }
-    ]
-};
+const { Content, Footer } = Layout;
 
 export const Home = () => {
+  const [t1Income, setT1Income] = useState([]);
+  const [handleIncome, setHandleIncome] = useState([]);
 
+  useEffect(()=> {
+    const fetchCharts = async () => {
+    const [{data: t1}, {data:handleBtc}] = await Promise.all([
+      axios.get(`https://raw.githubusercontent.com/imkgk/td/master/2020-04-22/short_margin_dollar.json`),
+      axios.get(`https://raw.githubusercontent.com/imkgk/td/master/2020-04-22/price.json`)
+    ]);
+    console.log(t1, handleBtc)
+    const startPriceByT1 = t1[0][1];
+    const startPriceByHandle = handleBtc[0][1]
+    const t1Income = t1.map(x=>[x[0], ((x[1]/startPriceByT1 - 1) * 100).toFixed(2)]);
+    const handleIncome = handleBtc.map(x=>[x[0], ((x[1]/startPriceByHandle - 1) * 100).toFixed(2)]);
+    setT1Income(t1Income);
+    setHandleIncome(handleIncome)
+  };
+
+  fetchCharts();
+  }, [])
+
+  const option = {
+      title: {
+          text: '收益曲线'
+      },
+      tooltip: {
+          trigger: 'axis',
+      },
+      legend: {
+        data: [{
+          name: '本产品',
+          // 强制设置图形为圆。
+          icon: 'circle',
+        },
+        {
+          name: '持有BTC',
+          // 强制设置图形为圆。
+          icon: 'circle',
+        }],
+        formatter: (name) =>{
+          const value = name === '本产品' ? t1Income : handleIncome;
+          return value.length === 0 ? `${name} +0.00%` : `${name} +${value[value.length -1][1]}%`
+        },
+        right: 0,
+        top: 3
+      },
+      grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+      },
+
+      xAxis: {
+          type: 'time',
+      },
+      yAxis: {
+          type: 'value',
+          axisLabel: {
+             show:true,
+             formatter:'{value} %'
+          },
+      },
+      series: [
+          {
+              name: '本产品',
+              type: 'line',
+              data: t1Income,
+              itemStyle: {
+                color: '#ED662C'
+              }
+          },
+          {
+              name: '持有BTC',
+              type: 'line',
+              data: handleIncome,
+              itemStyle: {
+                color: '#50B7F9'
+              }
+          },
+      ]
+  };
   return (
     <Layout>
-      <Header className="header">
-        <img src={logo} alt="logo" className="logo"/>
-      </Header>
       <Divider className="divider"/>
       <Content>
         <Row className="banner">
@@ -97,18 +117,63 @@ export const Home = () => {
                 <a href='/'>策略详情></a>
               </div>
             </Row>
-            <Row>
+            <Row style = {{padding: '20px 0'}} >
               <Col sm={16} xs={24} className="card-left">
+              {
                 <LineChart
                   option={option}
                 />
+              }
               </Col>
-              <Col sm={8} xs={24} className="card-right"></Col>
+              <Col sm={8} xs={24} className="card-right">
+                <div style={{width: '100%'}}>
+                <Row>
+                  <Col sm={12}>
+                    <p className="card-right-title">成立以来收益</p>
+                    <p className="card-right-content green size-30">1000%</p>
+                  </Col>
+                  <Col sm={12}>
+                    <p className="card-right-title">最新净值（05-10）</p>
+                    <p className="card-right-content"><span className="size-30">1.34</span> <span className="grey">USD</span></p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm={8}>
+                    <p className="card-right-title">日涨跌</p>
+                    <p className="card-right-content green">+10%</p>
+                  </Col>
+                  <Col sm={8}>
+                  <p className="card-right-title">近1月涨跌</p>
+                  <p className="card-right-content red">-16.4%</p>
+                  </Col>
+                  <Col sm={8}>
+                  <p className="card-right-title">近3月涨跌</p>
+                  <p className="card-right-content red">-10.4%</p>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col sm={8}>
+                    <p className="card-right-title">运行天数</p>
+                    <p className="card-right-content">1234</p>
+                  </Col>
+                  <Col sm={16}>
+                    <p className="card-right-title">交易标的</p>
+                    <p className="card-right-content">BitMEX XBTUSD</p>
+                  </Col>
+                </Row>
+                </div>
+              </Col>
             </Row>
           </Card>
         </Row>
+
+        <p className="more">更多产品 敬请期待</p>
       </Content>
-      <Footer></Footer>
+      <Footer className="footer">
+        <p className="footer-title">关于 TrendFund</p>
+        <p className="footer-subtitle">TrendFund 成立于2017年，专注于加密资产的量化交易研究。团队成员均为业内顶尖产品经理、分析师、交易员、工程师等。</p>
+        <img className="footer-icon" src={footerIcon} alt="footer details" draggable="false" />
+      </Footer>
     </Layout>
   )
 }
